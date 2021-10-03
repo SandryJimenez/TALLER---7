f81c930431e42f3d52ae900f4250ecd877b43214
SandryJimenez <90071133+SandryJimenez@users.noreply.github.com>
2021-10-02 21:46:26 -0500

    TALLER----7
    SANDRY JIMENEZ

---
+++ Taller7/src/PersonaPunto3/TestHerencia2.java
@@ -0,0 +1,15 @@
+
+
+package PersonaPunto3;
+
+/**
+ *
+ * @author LENOVO
+ */
+//public class Test2 {
+    //public static void main(String[] args) {
+        //ProfesorInterino P1 = new ProfesorInterino("NOV 22");
+        //System.out.println(P1.retornarDatos());
+    //}
+   
+//}
